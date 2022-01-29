862fb381ceac830f12bc18e3c728e01c793cb63b
Rohit-shedage <rohitshedage60@gmail.com>
2022-01-29 14:41:24 +0530

    Performing Integer Operation in python

---
+++ IDLE/Basic_Programs/Integer_Operation.py
@@ -0,0 +1,31 @@
+"""
+@Author : Rohit Shedage
+@Goal : To demonstrate basic operation on integer
+@Date : 26-10-2021
+"""
+
+n1 = 20
+n2 = 2
+
+Summation = n1 + n2   #Interger Addition
+
+Subtraction = n1 - n2  #Interger Subtration
+
+Multiplication = n1 * n2  #Integer Multiplication
+
+Quotient = n1 // n2   #Integer Quotient
+
+Remainder = n1 % n2   #Integer Remainder
+
+n1_Raised_n2 = n1 ** n2 #Exponential Operator
+
+f_div = n1 / n2  #Floating Point Division
+
+print("Summation :",Summation)
+print("Subtractiont :",Subtraction)
+print("Multiplication :",Multiplication)
+print("Quotient :",Quotient)
+print("Remainder :",Remainder)
+print("n1 Raised n2 :",n1_Raised_n2)
+print("f_Div :",f_div)
+print(n1 ** (1/2))
