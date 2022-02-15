0cb49dc4ac955c914f64a6203711394cf1dc997d
Rohit-shedage <rohitshedage60@gmail.com>
2022-02-15 22:53:24 +0530

    Summation of Two Number

---
+++ IDLE/Basic_Programs/Summation.py
@@ -0,0 +1,12 @@
+"""
+@Author :  Rohit Shedage
+@Goal : To sum of integer and display the result
+@Date : 26-10-2021
+"""
+
+n1 = 10;
+n2 = 20
+
+summation = n1 + n2
+
+print("Summation :",summation)
