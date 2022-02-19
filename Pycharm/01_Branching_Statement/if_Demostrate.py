0980c170ef894186e16b73461a51e2912d3f51b9
Rohit-shedage <rohitshedage60@gmail.com>
2022-02-19 13:38:47 +0530

    Branching Statement

---
+++ Pycharm/01_Branching_Statement/if_Demostrate.py
@@ -0,0 +1,17 @@
+"""
+@Author : Rohit Shedage
+@Goal : To Demostrate if Statement
+@Date : 27-10-2021
+"""
+
+str_n1 = input("Enter a Number :")
+n1 = int(str_n1)
+
+print("-" * 40)
+
+if n1 > 0:
+    print(n1,"Number is Greater Than Zero")
+
+print("-" * 40)
+print("Thank You")
+
